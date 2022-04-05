2f8425ceba416f30b22e3b5837e6f6240c9c138f
Vitor Tadeu Rocha <vitortr05@gmail.com>
2022-04-05 20:06:05 -0300

    Primeiro commit do projeto: ler dois inteiros informados pelo usuario.

---
+++ AulaGit.js
@@ -0,0 +1,9 @@
+function soma () {
+var n1, n2;
+n1 = document.getElementById ("primeiroNumero").value;
+n2 = document.getElementById ("segundoNumero").value;
+
+n1 = parseInt (n1);
+n2 = parseInt (n2);
+
+}
