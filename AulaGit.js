6d1c5a4b1289e05d6136226ed2a4bc9ccdbce1a2
Vitor Tadeu Rocha <102200168+trVitor@users.noreply.github.com>
2022-04-05 20:21:20 -0300

    Update AulaGit.js

--- AulaGit.js
+++ AulaGit.js
@@ -1,13 +1,7 @@
-function soma () {
+function soma() {
 var n1, n2;
-n1 = document.getElementById ("primeiroNumero").value;
-n2 = document.getElementById ("segundoNumero").value;
-
-n1 = parseInt (n1);
-n2 = parseInt (n2);
-
-soma = n1+n2;
-
-document.getElementById ("resposta").innerHTML = soma;
-
+n1 = parseInt(document.getElementById("primeiroNumero").value);
+n2 = parseInt(document.getElementById("segundoNumero").value);
+soma = n1 + n2;
+document.getElementById("resposta").innerHTML = soma;
 }
